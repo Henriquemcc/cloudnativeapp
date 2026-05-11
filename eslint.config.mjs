701c9133b09349c2b54944e2f207d1f2f3820274
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-11 16:28:44 -0300

    Configurando a integração do Eslint com o Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,45 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended"),
+export default defineConfig([
+  {
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+    ),
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
 
-        ecmaVersion: 6,
-        sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
     rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "single"],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+        },
+      ],
     },
-}]);
+  },
+]);
