d8b90f13c74816e319e6442585f237a261d05b19
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-13 15:24:47 -0300

    Adicionando teste

---
+++ frontend/_tests__/index.test.js
@@ -0,0 +1,11 @@
+import Home from "@/app/page";
+import { render, screen } from "@testing-library/react";
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+    const heading = screen.getByRole("heading", {
+      name: /Welcome to Cloud Native App!/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
