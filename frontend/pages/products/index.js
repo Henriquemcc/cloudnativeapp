a341196382c20d4c251566486b4b78ee23bdd704
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-15 08:33:23 -0300

    Implementando exibição dos produtos

--- frontend/pages/products/index.js
+++ frontend/pages/products/index.js
@@ -1,4 +1,9 @@
-import { List } from "@mui/material";
+import { useState } from "react";
+import TableBody from "@mui/material/TableBody";
+import List from "@mui/material/List";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+
 import PageHeader from "../../components/pagetemplate/PageHeader";
 import PageContent from "../../components/pagetemplate/PageContent";
 import PageContentLabels from "../../components/pagetemplate/PageContentLabels";
@@ -14,13 +19,38 @@ const itemsLabels = [
   "Actions",
 ];
 
+const productsList = [
+  { name: "P1", price: 1, category: "C1", count: 1, rating: 1, id: 1 },
+  { name: "P2", price: 2, category: "C2", count: 2, rating: 2, id: 2 },
+];
+
 export default function Products() {
+  const [products, setProducts] = useState(productsList);
+
   return (
     <List>
       <PageHeader pageLabel={pageLabel}></PageHeader>
       <PageContent>
         <PageContentLabels labels={itemsLabels} />
+        <PageContentItems products={products} />
       </PageContent>
     </List>
+  );
+}
+
+function PageContentItems({ products }) {
+  return (
+    <TableBody>
+      {products.map((product) => (
+        <TableRow key={product.id}>
+          <TableCell>{product.id}</TableCell>
+          <TableCell>{product.name}</TableCell>
+          <TableCell>{product.price}</TableCell>
+          <TableCell>{product.category}</TableCell>
+          <TableCell>{product.count}</TableCell>
+          <TableCell>{product.rating}</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
   );
 }
