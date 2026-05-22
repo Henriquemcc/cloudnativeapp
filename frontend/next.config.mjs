f3be4b6f68311ac2f6c2058b75cc97c28ba56305
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-22 12:18:48 -0300

    Definindo as variáveis host e port

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env: {
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+  }
 };
 
 export default nextConfig;
