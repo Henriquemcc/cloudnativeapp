d1ccbfdca45f8b67d5955aca493df262fe3f535b
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-25 18:56:22 -0300

    Configurando o index do router para usar o product router

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const rootRouter = require('./root.router');
+const productRouter = require('./product.router');
+
+router.use('/products', productRouter);
 
 router.use('/', rootRouter);
 
