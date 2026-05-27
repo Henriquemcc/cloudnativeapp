2aedab387d39e49b113d33c6cb0b8b5def6a1f31
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-27 17:56:35 -0300

    Revert "Configurando o models para utilizar a configuração do banco de dados"
    This reverts commit fd14542cf12116c43075246464fca19c5c463497.

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -5,9 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const config = require('../config/database');
 const env = process.env.NODE_ENV || 'development';
-//const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
