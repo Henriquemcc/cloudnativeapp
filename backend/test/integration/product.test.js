440943cc870dccbd48e1d217e01975d8fd7eeb04
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-27 16:43:49 -0300

    Implementando caso de teste

--- backend/test/integration/product.test.js
+++ backend/test/integration/product.test.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest');
 const app = require('../../src/app');
 const request = supertest(app);
+const { Product } = require('../../src/models');
 
 test('Get products', async() => {
     const response = await request.get('/products');
@@ -19,4 +20,18 @@ test('Insert product', async() => {
     const response = await request.post('/products').send(payload);
     expect(response.status).toBe(200);
     expect(response.text).toBe('Product inserted sucessfully');
+});
+
+test('Delete product', async() => {
+    const productData = {
+        name: 'Product to be deleted',
+        price: 1.1,
+        category: 'Category to be deleted',
+        count: 10,
+        rating: 1.1,
+    };
+    let product = await Product.create(productData);
+    const response = await request.delete(`/products/${product.id}`);
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Product deleted sucessfully');
 });
