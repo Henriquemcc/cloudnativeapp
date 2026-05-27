20c5b5f4d5d2ba0e0df5d03d1be909efaba9a86c
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-27 16:30:11 -0300

    Alterando caso de teste para que a resposta esperada seja uma lista vazia

--- backend/test/integration/product.test.js
+++ backend/test/integration/product.test.js
@@ -5,5 +5,5 @@ const request = supertest(app);
 test('Get products', async() => {
     const response = await request.get('/products');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Products list');
+    expect(response.text).toBe('[]');
 });
