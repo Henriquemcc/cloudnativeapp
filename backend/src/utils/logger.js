035a8d990c6373643a164fa750da1a275438e748
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-29 14:05:55 -0300

    Adicionando função formatter

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { createLogger, config, transports } = require("winston");
+const { createLogger, config, transports, format } = require("winston");
 
 const transportConsole = new transports.Console({
   handleExceptions: true,
@@ -10,6 +10,16 @@ const transportFile = new transports.File({
   filename: "tmp/backend.log",
   handleExceptions: true,
 });
+
+const formatter = format.combine(
+  format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  format.splat(),
+  format.printf((info) => {
+    const { timestamp, level, message, ...meta } = info;
+
+    return `${timestamp} [${level}]: ${message} ${Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ""}`;
+  }),
+);
 
 class Logger {
   constructor() {
