a6da62fb62b79058176ae236ebaadfc3da197b77
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-29 14:24:27 -0300

    Formatando o código fonte

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const { logger } = require('./utils/logger');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { logger } = require("./utils/logger");
 const app = express();
-const router = require('./routers');
+const router = require("./routers");
 
 app.use(express.json());
 app.use(
-    morgan('combined', { stream: { write: (message) => { logger.info(message) }} })
+  morgan("combined", {
+    stream: {
+      write: (message) => {
+        logger.info(message);
+      },
+    },
+  }),
 );
 
 var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200,
+  origin: "*",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
-app.use('/', router);
+app.use("/", router);
 
 module.exports = app;
