2adfce3cfa65244c88bbffe8f0c051483ba1beee
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-29 14:27:34 -0300

    Alterando o caso de teste de logger

--- backend/test/unit/logger.test.js
+++ backend/test/unit/logger.test.js
@@ -1,5 +1,8 @@
 const logger = require('../../src/utils/logger');
 
 test('Test logger call', async () => {
-  logger.info('This is a test');
+  logger.debug('This is a log debug level');
+  logger.info('This is a log info level');
+  logger.warning('This is a log warning level');
+  logger.error('This is a log error level');
 });
