8d49dab4290a75805cf7ac2213735c494ae93db2
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-05-29 14:20:48 -0300

    Corrigindo erro de execução do servidor

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 class Config {
   static loadConfig() {
-    if (process.env.NODE_ENV === 'undefined') {
+    if (!process.env.NODE_ENV) {
       process.env.NODE_ENV = 'development';
     }
     this.envPath = `${process.cwd()}/.env.${process.env.NODE_ENV}`;
